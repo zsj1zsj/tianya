e496175d4d8cc2334ed8555f2e109a69d1fb6e3d
zsj1zsj <zsj1zsj@gmail.com>
2021-11-16 17:10:24 +0800

    Update TianyaAPI.java
    modify css selector to variables

--- TianyaAPI.java
+++ TianyaAPI.java
@@ -13,6 +13,14 @@ import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 
 public class TianyaAPI {
+    // tianya header:including title, author, date, word count
+    static String SEL_TITLE = "td:eq(1) > a";
+    static String SEL_AUTHOR = "td:eq(2)";
+    static String SEL_DATE = "td:eq(3)";
+    static String SEL_WORDCNT = "td:eq(4)";
+    static String SEL_PAGENUM = "div.page li > a";
+    static String SEL_ARTLISTPERPAGE = "table.listTable > tbody > tr";
+
     //https://tuoshuidu.com/c/tianya/%E7%85%AE%E9%85%92%E8%AE%BA%E5%8F%B2/
     static List<ArticleHeader> fetchPagelistByNo(String url) {
         Document doc = null;
@@ -21,36 +29,34 @@ public class TianyaAPI {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Elements newsHeadlines = doc.select("table.listTable > tbody > tr");
+        Elements newsHeadlines = doc.select(SEL_ARTLISTPERPAGE);
 //        System.out.println(newsHeadlines.size());
 
         return newsHeadlines.stream().skip(1).map(
                 h -> new ArticleHeader(
-                        h.select("td:eq(1) > a").text(),
-                        h.select("td:eq(2)").text(),
-                        h.select("td:eq(3)").text(),
-                        Integer.parseInt(h.select("td:eq(4)").text())
+                        h.select(SEL_TITLE).text(),
+                        h.select(SEL_AUTHOR).text(),
+                        h.select(SEL_DATE).text(),
+                        Integer.parseInt(h.select(SEL_WORDCNT).text())
                 )
         ).collect(toList());
     }
 
-    //String url = "https://tuoshuidu.com/c/tianya/%E7%85%AE%E9%85%92%E8%AE%BA%E5%8F%B2/"
-    static List<ArticleHeader> allPages(String url ) throws IOException {
-        ;
+    // String url = "https://tuoshuidu.com/c/tianya/%E7%85%AE%E9%85%92%E8%AE%BA%E5%8F%B2/"
+    static List<ArticleHeader> allPages(String url) throws IOException {
         Document doc = Jsoup.connect(url).get();
 
-        Element pageNumEle = doc.select("div.page li > a").last();
+        Element pageNumEle = doc.select(SEL_PAGENUM).last();
         System.out.println(pageNumEle.text());
         int pageNum = Integer.parseInt(pageNumEle.text());
 
         List<String> pageUrls = IntStream.range(1, pageNum + 1).boxed().map(i -> url + i + ".html").collect(toList());
 
-        pageUrls.stream().limit(pageNum).map(TianyaAPI::fetchPagelistByNo)
+        pageUrls.stream().limit(10).map(TianyaAPI::fetchPagelistByNo)
                 .flatMap(x -> x.stream())
-                .sorted(comparing(ArticleHeader::getRating).reversed())
+                .sorted(comparing(ArticleHeader::getWordCnt).reversed())
                 .limit(3)
                 .forEach(System.out::println);
-
         return null;
     }
 }
