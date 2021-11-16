3f9af8bbf2b7fb7372182fd1f29d40a28a15bcf0
zsj1zsj <zsj1zsj@gmail.com>
2021-11-16 17:21:29 +0800

    Update TianyaAPI.java
    add parallel for function allPages

--- TianyaAPI.java
+++ TianyaAPI.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 
 public class TianyaAPI {
@@ -50,13 +49,14 @@ public class TianyaAPI {
         System.out.println(pageNumEle.text());
         int pageNum = Integer.parseInt(pageNumEle.text());
 
-        List<String> pageUrls = IntStream.range(1, pageNum + 1).boxed().map(i -> url + i + ".html").collect(toList());
-
-        pageUrls.stream().limit(10).map(TianyaAPI::fetchPagelistByNo)
+        return IntStream.range(1, pageNum + 1).boxed()
+                .map(i -> url + i + ".html")
+                .parallel()
+//                .limit(100)
+                .map(TianyaAPI::fetchPagelistByNo)
                 .flatMap(x -> x.stream())
-                .sorted(comparing(ArticleHeader::getWordCnt).reversed())
-                .limit(3)
-                .forEach(System.out::println);
-        return null;
+//                .sorted(comparing(ArticleHeader::getWordCnt).reversed())
+//                .limit(3)
+                .collect(toList());
     }
 }
