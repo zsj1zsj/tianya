fd2c4503a0f67cedf3d381f646d3f9307ceb5d1f
zsj1zsj <zsj1zsj@gmail.com>
2021-11-18 13:18:27 +0800

    Update DateUtil.java
    update logics

--- DateUtil.java
+++ DateUtil.java
@@ -19,19 +19,30 @@ public class DateUtil {
         return ChronoUnit.WEEKS.between(d1, d2);
     }
 
+
     public static void main(String[] args) {
         int startNo = 9226;
         LocalDate startDate = LocalDate.of(2021, 1, 2);
 
-        String mp3 = "http://audiocdn.economist.com/sites/default/files/AudioArchive/2021/%s/Issue_%s_%s_The_Economist_Full_edition.zip";
+        String mp3 = "http://audiocdn.economist.com/sites/default/files/AudioArchive/#{DateYear}/#{date}/Issue_#{no}_#{date}_The_Economist_Full_edition.zip";
         String mobi = "https://raw.githubusercontent.com/hehonghui/the-economist-ebooks/master/01_economist/te_%s/TheEconomist.%s.mobi";
 
         LocalDate today = LocalDate.now();
         LocalDate lastestDate = today.minusDays(betweenDays(startDate, today) % 7);
-        String lastestDateMp3 = lastestDate.format(DateTimeFormatter.ofPattern("YYYYMMDD"));
-        System.out.format(mp3, lastestDateMp3, betweenWeeks(startDate, today) + startNo, lastestDateMp3);
-        String latestDateMobi = lastestDate.format(DateTimeFormatter.ofPattern("YYYY.MM.DD"));
-        System.out.println();
-        System.out.format(mobi, latestDateMobi, latestDateMobi);
+//        System.out.println(lastestDate);
+        String lastestDateMp3 = lastestDate.format(DateTimeFormatter.ofPattern("YYYYMMdd"));
+//        System.out.println(lastestDateMp3);
+//        System.out.format(mp3, lastestDateMp3, betweenWeeks(startDate, today) + startNo, lastestDateMp3);
+        String latestDateMobi = lastestDate.format(DateTimeFormatter.ofPattern("YYYY.MM.dd"));
+//        System.out.println();
+        System.out.println((new StringPlaceholder(mp3)
+                .arg("no",String.valueOf(betweenWeeks(startDate,today)+startNo))
+                .arg("date",lastestDateMp3)
+                .arg("DateYear",lastestDate.format(DateTimeFormatter.ofPattern("YYYY")))
+                .build()
+        ));
+
+//        System.out.println();
+//        System.out.println(lastestDate.format(DateTimeFormatter.ofPattern("YYYY-MM-D")));
     }
 }
